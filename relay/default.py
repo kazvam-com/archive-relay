7d57973872458d2138d10388ae840542c13ddb67
Rumen Damyanov <rumen.damyanov@gmail.com>
2019-02-15 12:44:51 +0200

    updated default.py

--- relay/default.py
+++ relay/default.py
@@ -15,20 +15,33 @@ async def default(request):
         content_type="text/html",
         charset="utf-8",
         text="""
-<html><head>
+<!DOCTYPE html>
+<html lang='en'><head>
+ <meta charset='utf-8'>
+ <link href='https://kazvam.com/kaz-favicon.png' rel='icon' type='image/x-icon'>
+ <link href='https://relay.kazvam.com/' rel='canonical'>
  <title>ActivityPub Relay at {host}</title>
- <style>
-  p {{ color: #FFFFFF; font-family: monospace, arial; font-size: 100%; }}
+ <meta name='author' content='Roumen Damianoff (damianoff.com)'>
+ <meta name='publisher' content='Kazvam.com (kazvam.com)'>
+ <meta name='generator' content='Kazvam.com (kazvam.com)'>
+ <meta name='description' content='ActivityPub (Mastodon/Pleroma) relay server hosted by Kazvam.com'>
+ <meta name='keywords' content='ActivityPub relay, kazvam, mastodon, pleroma'>
+ <meta name='robots' content='index, follow, noodp, noydir'>
+<style>
+  h1,h2,p {{ color: #FFFFFF; font-family: monospace, arial; font-size: 100%; }}
   body {{ background-color: #000000; }}
   </style>
 </head>
 <body>
-<p>This is an Activity Relay for fediverse instances.</p>
-<p>{note}</p>
+<section>
+<h1>{note}</h1>
+<h2>This is an Activity Relay for fediverse instances.</h2>
 <p>For Mastodon instances, you may subscribe to this relay with the address: <a href="https://{host}/inbox">https://{host}/inbox</a></p>
 <p>For Pleroma and other instances, you may subscribe to this relay with the address: <a href="https://{host}/actor">https://{host}/actor</a></p>
-<p>To host your own relay, you may download the code at this address: <a href="https://git.pleroma.social/pleroma/relay">https://git.pleroma.social/pleroma/relay</a></p>
-<br><p>List of {count} registered instances:<br>{targets}</p>
+<p>To host your own relay, you may download the code at this address: <a href="https://gitlab.com/Kazvam/relay">https://gitlab.com/Kazvam/relay</a></p>
+<br><p>List of {count} registered instances:<br><br><b>{targets}</b></p>
+<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" alt="kazvam logo" style="display:none;">
+</section>
 </body></html>
 
 """.format(host=host, note=note,targets=targets,count=len(inboxes)))
